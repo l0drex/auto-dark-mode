01ac7de43a3d7922899b65270b79f3a246a65dfe
l0drex <l0drex@pm.me>
2022-04-17 00:37:34 +0200

    Rebase on master

--- linux/xdg-desktop-portal/src/main/java/com/github/weisj/darkmode/platform/linux/xdg/XdgThemeMonitorService.kt
+++ linux/xdg-desktop-portal/src/main/java/com/github/weisj/darkmode/platform/linux/xdg/XdgThemeMonitorService.kt
@@ -11,25 +11,25 @@ import org.freedesktop.dbus.types.Variant
 
 @DBusInterfaceName("org.freedesktop.portal.Settings")
 interface FreedesktopInterface : DBusInterface {
-    fun read(namespace: String, key: String): Variant <*>
+    fun Read(namespace: String, key: String): Variant <*>
 }
 
 class XdgThemeMonitorService : ThemeMonitorService {
     private val connection = DBusConnection.getConnection(DBusConnection.DBusBusType.SESSION)
-    private val freedesktopInterface = connection.getRemoteObject(
+    private val freedesktopInterface : FreedesktopInterface = connection.getRemoteObject(
         "org.freedesktop.portal.Desktop",
-        "/org/freedesktop/portal/Desktop",
+        "/org/freedesktop/portal/desktop",
         FreedesktopInterface::class.java
     )
 
+    private val themeMode: Number
+        get() {
+            val theme = freedesktopInterface.Read("org.freedesktop.appearance", "color-scheme")
+            return recursiveVariantValue(theme) as Number
+        }
+
     override val isDarkThemeEnabled: Boolean
         get() {
-            val theme = freedesktopInterface.read("org.freedesktop.appearance", "color-scheme")
-            val themeMode = 0  //recursiveVariantValue(theme)
-            println(theme)
-
-            // 0: no preference (defaulting to dark), 1: dark, 2: light
-            // See https://github.com/Exalm/xdg-desktop-portal/blob/93d615d36a8ec98d1657a2342652f108546619f6/data/org.freedesktop.impl.portal.Settings.xml#L39
             return themeMode != 2
         }
     override val isHighContrastEnabled: Boolean

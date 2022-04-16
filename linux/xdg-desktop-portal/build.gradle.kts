e84ad154d59305df00e794b30ec0fd8ae6b42e2b
l0drex <l0drex@pm.me>
2022-04-17 01:04:49 +0200

    Remove old todo and unneeded dependency

--- linux/xdg-desktop-portal/build.gradle.kts
+++ linux/xdg-desktop-portal/build.gradle.kts
@@ -8,9 +8,7 @@ dependencies {
     implementation(projects.autoDarkModeBase)
     implementation(libs.darklaf.nativeUtils)
 
-    // TODO add xdg-desktop-portal dependency
     implementation("com.github.hypfvieh:dbus-java:3.3.1")
-    // implementation("com.github.hypfview:dbus-java-transport-jnr-unixsocket:4.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.1")
 
     kapt(libs.autoservice.processor)
